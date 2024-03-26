fde122d27f93844954100278223c1f325b163f60
Joseph Zemmels <jzemmels@usgs.gov>
2024-03-25 18:24:20 -0600

    Add use_file_usgs non-exported function

--- R/use_project_usgs.R
+++ R/use_project_usgs.R
@@ -8,17 +8,32 @@
 
 use_gitignore_usgs <- function(home = ".", additions = NULL){
 
-  cli::cli_inform(c("i" = "Using .gitignore from {.url https://code.usgs.gov/water/IWAAs-trends/templates/standard-template-repository/-/blob/master/.gitignore?ref_type=heads}"))
+  use_file_usgs(inst_file = "gitignore.txt",
+                out_file = ".gitignore",
+                home = home,
+                additions = additions)
 
-  git_ignore_path <- file.path(home,".gitignore")
+  return(invisible(NULL))
 
-  if(!file.exists(git_ignore_path)) file.create(git_ignore_path)
+}
 
-  cat(c(readLines(system.file("gitignore.txt",package = "mortar")), additions),
-      file = git_ignore_path,
+#' internal function to be used in other use_*_usgs functions, which all have
+#' the same basic structure
+#' @noRd
+use_file_usgs <- function(inst_file,
+                          out_file = stringr::str_remove(inst_file, "\\.txt"),
+                          home = ".",
+                          additions = NULL){
+  cli::cli_inform(c("i" = "Using {.file {out_file}} from {.url https://code.usgs.gov/water/IWAAs-trends/templates/standard-template-repository/}"))
+
+  out_file_path <- file.path(home, out_file)
+
+  if(!file.exists(out_file_path)) file.create(out_file_path)
+
+  cat(c(readLines(system.file(inst_file, package = "mortar")), additions),
+      file = out_file_path,
       sep = "\n",
       fill = FALSE)
 
   return(invisible(NULL))
-
 }
